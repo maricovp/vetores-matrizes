46e3970cb1f370e2b60e5aecd0bd332041f10363
maricovp <marcos2015vinicuis@gmail.com>
2024-04-10 10:57:36 -0400

    adicionado atividade5

---
+++ atividade5.js
@@ -0,0 +1,9 @@
+/*Questão 5: Crie uma matriz 3x3 e imprima o elemento na terceira linha e segunda
+coluna em JavaScript?*/
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+let elemento = matriz[2][1];
+console.log(elemento);
