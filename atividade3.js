348105b49a889ad857ca27d681342214f576aafa
maricovp <marcos2015vinicuis@gmail.com>
2024-04-10 10:57:25 -0400

    adicionado atividade3

---
+++ atividade3.js
@@ -0,0 +1,5 @@
+/*Crie um vetor com 10 elementos e remova o último elemento e imprima
+a matriz sem o elemento removido em JavaScript*/
+let Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+Array.pop();
+console.log(Array);
