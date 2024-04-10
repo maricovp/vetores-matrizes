63e8ecb9db7ae2c462a9ed982a878362a4342e56
maricovp <marcos2015vinicuis@gmail.com>
2024-04-10 10:56:59 -0400

    adicionado matriz atvs

---
+++ matriz.js
@@ -0,0 +1,17 @@
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8,9]
+];
+
+console.log(matriz[0][0]);
+console.log[1][2];
+matriz[0][0] = 10;
+
+console.log(matriz);
+
+ for (let i = 0; i< matriz.length; i++){
+ for(let j = 0; j < matriz[i].length; j++) {
+    console.log(matriz[i][j])
+   }
+ }
